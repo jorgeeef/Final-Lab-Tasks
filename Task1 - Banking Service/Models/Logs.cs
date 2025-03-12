cafbf7e80b5ca0c6acb58affe78e7845f55ef909
Georges FARDISSI <fardissi.georges@gmail.com>
2025-03-12 15:49:10 +0200

    Adding logs model

---
+++ Task1 - Banking Service/Models/Logs.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task1___Banking_Service.Models;
+
+public class Logs
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; } 
+
+    [Required]
+    public Guid RequestId { get; set; }
+    
+    [Column(TypeName = "jsonb")]
+    public string RequestObject { get; set; }  
+
+    [Required]
+    public string RouteURL { get; set; }  
+
+    [Required]
+    public DateTime Timestamp { get; set; }  
+}
