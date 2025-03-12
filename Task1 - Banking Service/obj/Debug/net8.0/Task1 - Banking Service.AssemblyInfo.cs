aec4ef59ab03168b26750ca8ce40cd418f22a230
Georges FARDISSI <fardissi.georges@gmail.com>
2025-03-12 15:48:03 +0200

    First commit

--- Task1 - Banking Service/obj/Debug/net8.0/Task1 - Banking Service.AssemblyInfo.cs
+++ Task1 - Banking Service/obj/Debug/net8.0/Task1 - Banking Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1 - Banking Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2c37934d8912e41386904b142e61fc161284ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcbb9be0f38504e7f4b6104799510d5c1ff427fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1 - Banking Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1 - Banking Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
