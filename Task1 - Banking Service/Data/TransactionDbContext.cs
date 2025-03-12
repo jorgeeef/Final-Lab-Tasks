1abdacb53af26627dc383cd76953c40d9578297e
Georges FARDISSI <fardissi.georges@gmail.com>
2025-03-12 14:19:42 +0200

    Database Setup

---
+++ Task1 - Banking Service/Data/TransactionDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task1___Banking_Service.Models;
+
+namespace Task1___Banking_Service.Data;
+
+public class TransactionDbContext: DbContext
+{
+    public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options) { }
+
+    public DbSet<TransactionLog> TransactionLogs { get; set; }
+}
---
+++ Task1 - Banking Service/Models/TransactionLog.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task1___Banking_Service.Models;
+
+public class TransactionLog
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+    
+    public long AccountId { get; set; }
+    
+    public string TransactionType { get; set; } 
+    
+    public decimal Amount { get; set; }
+    
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    
+    public string Status { get; set; } // (Pending, Completed, Failed)
+    
+    public string Details { get; set; }
+}
