9a0c5be1fc0aa14a2973905b2c096ed5a0f82682
Georges FARDISSI <fardissi.georges@gmail.com>
2025-03-12 16:19:33 +0200

    Adding TransactionEvent Model

---
+++ Task1 - Banking Service/Models/TransactionEvent.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task1___Banking_Service.Models;
+
+public class TransactionEvent
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long Id { get; set; }
+        
+    [Required]
+    public long TransactionId { get; set; } 
+        
+    [Required]
+    public string EventType { get; set; } // "Deposit", "Withdrawal" or...
+        
+    [Required]
+    public string Details { get; set; } 
+        
+    [Required]
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow; 
+}
